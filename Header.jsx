f9aed2526012e9d4c1231c8f65b43aff90712290
imJeetrathod <85473379+imJeetrathod@users.noreply.github.com>
2021-06-07 11:53:33 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App () {
+  return (
+
+   <header>
+      <h1>ShapeAI</h1>
+   </header>
+);
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+ 
+  return (
+   <div className="note">
+     <h1>Javascript and React.js</h1>
+     <p> This was an amazing bootcamp taken by shaurya sinha we coverd everything from scratch including Javascript React.ja,HTML</p>
+   </div>
+   
+);
+}
+
+export default Note;
